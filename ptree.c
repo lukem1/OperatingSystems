63e328988a9a85631ffc12764880d928dc1e77be
lukem1 <37024616+lukem1@users.noreply.github.com>
2020-02-04 14:07:58 -0500

    made new things broke some things

--- ptree.c
+++ ptree.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <dirent.h>
 
-// Structure to keep track of processes and their relationships
-struct process_old
-{
-	int pid; // Process id
-	int ppid; // Parent process id
-	int vsize; // Size of virtual address space
-	char **exe; // Pointer to name of exe TODO: may be incorrect type
-	
-	struct process *parent; // Pointer to parent
-	struct process *children[]; // Array of pointers to children
-};
-
+// Structure to hold process data
 struct process
 {
 	int pid;
 	int ppid;
 	unsigned long vsize;
-	char **name;
-	
-	struct process *next;
-	struct process *prev;
+	char **pname;
+};
+
+// Prints a list of proccess structs
+void printList(struct process list[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("pid: %d, ppid: %d, name: %s, memory: %lu (kb)\n", list[i].pid, list[i].ppid, list[i].pname, list[i].vsize/1000);
+	}
 }
 
 // Determines if a string is a number
@@ -59,24 +54,40 @@ int main(int argc, char *argv[])
 	struct dirent *dir;
 	DIR *proc = opendir("/proc");
 	
+	int procCount = 0;
+	while ((dir = readdir(proc)) != NULL)
+	{
+		if (isNumber(dir->d_name)) { procCount += 1; }
+	}
+	closedir(proc);
+	
+	struct process procList[procCount];
+	
+	int c = 0;
+	proc = opendir("/proc");
 	while ((dir = readdir(proc)) != NULL)
 	{
 		if (isNumber(dir->d_name)) { 
-			printf("Process: "); 
-			printf("%s\n", dir->d_name);
+			//printf("Process: %s\n", dir->d_name); 
 			int pid;
-			char *name[32];
+			char *pname[32];
 			int ppid;
 			unsigned long vsize;
 			char path[267];
 			sprintf(path, "/proc/%s/stat", dir->d_name);
 			FILE *stream = fopen(path, "r");
-			fscanf(stream, "%d %s %*s %d %*d %*d %*d %*d %*u %*u %*u %*u %*u %*u %*u %*d %*d %*d %*d %*d %*d %*u %lu", &pid, name, &ppid, &vsize);
-			//fscanf(stream, "%d %s %*s %d ", &pid, name, &ppid);
-			printf("pid: %d, ppid: %d, name: %s, memory: %lu (kb)\n", pid, ppid, name, vsize/1000);
+			fscanf(stream, "%d %s %*s %d %*d %*d %*d %*d %*u %*u %*u %*u %*u %*u %*u %*d %*d %*d %*d %*d %*d %*u %lu", &pid, pname, &ppid, &vsize);
+			printf("pid: %d, ppid: %d, name: %s, memory: %lu (kb)\n", pid, ppid, pname, vsize/1000);
+			procList[c].pid = pid;
+			procList[c].ppid = ppid;
+			procList[c].pname = pname;
+			procList[c].vsize = vsize;
+			c += 1;
 		}
 	}
-	
+	printf("Head of process list:\n");
+	printf("pid: %d, ppid: %d, name: %s, memory: %lu (kb)\n", procList[0].pid, procList[0].ppid, procList[0].pname, procList[0].vsize/1000);
+	printList(procList, procCount);
 	closedir(proc);
 	return 0;
 }
