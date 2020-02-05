f9328c65d927ee9e7627e0cd64291f13b67e3d14
lukem1 <37024616+lukem1@users.noreply.github.com>
2020-02-04 20:18:06 -0500

    comments + fixed warnings

--- ptree.c
+++ ptree.c
@@ -1,34 +1,37 @@
+//
+// ptree.c
+//
+// Luke McGuire
+// Matt Walter
+//
+// 4 February 2020
+//
+
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
-// Structure to hold process data
+// Structure to hold process information
 struct process
 {
-	int pid;
-	int ppid;
-	unsigned long vsize;
-	char pname[32];
+	int pid; // Process ID
+	int ppid; // Parent process ID
+	unsigned long vsize; // Virtual address space size (bytes)
+	char pname[32]; // Name of executable
 };
-
-// Prints a list of proccess structs
-void printList(struct process list[], int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		printf("pid: %d, ppid: %d, name: %s, memory: %lu (kb)\n", list[i].pid, list[i].ppid, list[i].pname, list[i].vsize/1000);
-	}
-}
 
 // Recursivley prints the processes as a tree
 void printTree(struct process list[], int size, int depth, int ppid)
 {
+	// Find processes with a parent process ID of ppid
 	for (int i = 0; i < size; i++)
 	{
 		if (list[i].ppid == ppid)
 		{
 			for (int j = 0; j < depth; j++)
 				printf("  ");
-			printf("(%d) %s, %d kb\n", list[i].pid, list[i].pname, list[i].vsize/1000);
+			printf("(%d) %s, %lu kb\n", list[i].pid, list[i].pname, list[i].vsize/1000);
+			// Recursive call to find and print children of current process
 			printTree(list, size, depth+1, list[i].pid); 
 		}
 	}
@@ -50,25 +53,21 @@ int isNumber(char s[])
 				break;
 			}
 		}
-		if (!found)
+		if (!found) // Found non numeric character
 		{
-			//printf("Not Found: %c\n", s[i]);
 			return 0;
 		}
 	}
-	return 1;
+	return 1; // All numeric characters
 }
 
 int main(int argc, char *argv[])
 {
-	// TODO kb not bytes
-	//char *a = *(argv + 1);
-	//printf("argv: %s\n", a);
-	//printf("Number?: %d\n", isNumber(a));
 	
 	struct dirent *dir;
-	DIR *proc = opendir("/proc");
 	
+	// Read proc and count processes
+	DIR *proc = opendir("/proc");
 	int procCount = 0;
 	while ((dir = readdir(proc)) != NULL)
 	{
@@ -76,35 +75,36 @@ int main(int argc, char *argv[])
 	}
 	closedir(proc);
 	
+	// Array of processes sized based on counted processes
 	struct process procList[procCount];
 	
+	// Read in process information and store in array
 	int c = 0;
 	proc = opendir("/proc");
 	while ((dir = readdir(proc)) != NULL)
 	{
-		if (isNumber(dir->d_name)) { 
-			//printf("Process: %s\n", dir->d_name); 
+		if (isNumber(dir->d_name))
+		{ 
 			int pid;
-			char *pname[32];
+			char pname[32];
 			int ppid;
 			unsigned long vsize;
 			char path[267];
 			sprintf(path, "/proc/%s/stat", dir->d_name);
 			FILE *stream = fopen(path, "r");
+			// Scan /proc/pid/stat for process info
 			fscanf(stream, "%d %s %*s %d %*d %*d %*d %*d %*u %*u %*u %*u %*u %*u %*u %*d %*d %*d %*d %*d %*d %*u %lu", &pid, pname, &ppid, &vsize);
-			//printf("pid: %d, ppid: %d, name: %s, memory: %lu (kb)\n", pid, ppid, pname, vsize/1000);
+			// Store process info in process struct in the array
 			procList[c].pid = pid;
 			procList[c].ppid = ppid;
 			strcpy(procList[c].pname, pname);
-			//procList[c].pname = &pname;
 			procList[c].vsize = vsize;
 			c += 1;
 		}
 	}
-	printf("Head of process list:\n");
-	printf("pid: %d, ppid: %d, name: %s, memory: %lu (kb)\n", procList[0].pid, procList[0].ppid, procList[0].pname, procList[0].vsize/1000);
 	closedir(proc);
 	
+	// Call recursive function to print process tree
 	printTree(procList, procCount, 0, 0);
 	
 	return 0;
